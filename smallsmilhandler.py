3dee76db245d3d7eb5b3773081dfb9206e183260
ahmedabd <ahmedabdulkarim06@gmail.com>
2015-10-19 13:19:28 +0200

    smallsmilhandler.py

---
+++ smallsmilhandler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+    def __init__(self):
+        self.lista = []
+        self.diccionario = {
+            "root-layout": ["width", "height", "background-color"],
+            "region": ["id", "top", "botton", "left", "rigth"],
+            "img": ["src", "region", "begin", "dur"],
+            "audio": ["src", "begin", "dur"],
+            "textstream": ["src", "region"]
+        }
+
+    def startElement(self, name, attrs):
+        if name in self.diccionario:
+            Mi_Dicc = {}
+            for atributo in self.diccionario[name]:
+                Mi_Dicc[atributo] = attrs.get(atributo, "")
+            self.lista.append(Mi_Dicc)
+
+    def get_tags(self):
+        return self.lista
+
+if __name__ == "__main__":
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    lista = cHandler.get_tags()
+    print(lista)
